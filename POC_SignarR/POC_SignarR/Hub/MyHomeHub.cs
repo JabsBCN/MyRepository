6748185498c1913d8832e5867e0dbb8a37c409fb
Xavier Garcia <xgarcia@externo.wke.es>
2015-09-21 17:16:26 +0200

    Comment added.

--- POC_SignarR/POC_SignarR/Hub/MyHomeHub.cs
+++ POC_SignarR/POC_SignarR/Hub/MyHomeHub.cs
@@ -17,6 +17,11 @@
             return base.OnConnected();
         }
 
+        /// <summary>
+        /// On disconnected event.
+        /// </summary>
+        /// <param name="stopCalled"></param>
+        /// <returns></returns>
         public override Task OnDisconnected(bool stopCalled)
         {
             string removeName;
@@ -25,11 +30,20 @@
             return base.OnDisconnected(stopCalled);
         }
 
+        /// <summary>
+        /// Send a message to all clients.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="userName"></param>
         public void SendMessageToClients(string message, string userName)
         {
-            Clients.All.OnMessageReceived(message, userName);66
+            Clients.All.OnMessageReceived(message, userName);
         }
 
+        /// <summary>
+        /// Send user's names connected to all clients.
+        /// </summary>
+        /// <param name="userName"></param>
         public void SendUserNameConnectToClients(string userName)
         {
             conexiones.TryAdd(Context.ConnectionId, userName);
