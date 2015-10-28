10bf6dea8a0257b53b812cc7eb3aa6fc713f193c
Xavier Garcia <xgarcia@externo.wke.es>
2015-10-28 17:00:17 +0100

    BillingSystem.00 solution created.

---
+++ BillingSystem.00/BillingSystem.Test/BillingDoohickeyTests.cs
@@ -0,0 +1,13 @@
+﻿namespace BillingSystem.Test
+{
+    using Xunit;
+    using Moq;
+
+    public class BillingDoohickeyTests
+    {
+        [Fact]
+        public void UnitTest1()
+        {
+        }
+    }
+}
