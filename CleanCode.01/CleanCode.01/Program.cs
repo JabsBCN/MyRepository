39c3f3cc137ad7823315452f7e802af3586813ff
Xavier Garcia <xgarcia@externo.wke.es>
2016-03-01 16:34:14 +0100

    CleanCode.01 project added.

---
+++ CleanCode.01/CleanCode.01/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Clean.Code
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static IEnumerable<Guid> GetThen(Dictionary<Guid, int> list1)
+        {
+            var list2 = new List<Guid>();
+
+            foreach (var x in list1)
+            {
+                if (x.Value == 4)
+                {
+                    list2.Add(x.Key);
+                }
+            }
+
+            return list2;
+        }
+    }
+}
