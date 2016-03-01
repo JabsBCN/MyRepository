6748ad0533cebd7dc3ec3323d63ad6e94b999359
Xavier Garcia <xgarcia@externo.wke.es>
2016-03-01 17:29:41 +0100

    Print data.

--- CleanCode.01/CleanCode.01/Program.cs
+++ CleanCode.01/CleanCode.01/Program.cs
@@ -7,21 +7,42 @@
     {
         static void Main(string[] args)
         {
+            Dictionary<int, string> list1 = GetSampleData();
+
+            var list2 = GetThen(list1);
+
+            foreach (int item in list2)
+            {
+                Console.WriteLine("Cuenta: {0} - Activa", item);
+            }
         }
 
-        public static IEnumerable<Guid> GetThen(Dictionary<Guid, int> list1)
+        public static IEnumerable<int> GetThen(Dictionary<int, string> list1)
         {
-            var list2 = new List<Guid>();
+            var list2 = new List<int>();
 
             foreach (var x in list1)
             {
-                if (x.Value == 4)
+                if (x.Value == "1")
                 {
                     list2.Add(x.Key);
                 }
             }
 
             return list2;
+        }
+
+        private static Dictionary<int, string> GetSampleData()
+        {
+            Dictionary<int, string> list = new Dictionary<int, string>();
+
+            list.Add(1, "3");
+            list.Add(2, "4");
+            list.Add(3, "1");
+            list.Add(4, "2");
+            list.Add(5, "1");
+
+            return list;
         }
     }
 }
