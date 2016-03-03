808677acc1bc76acaca0fa49aa193289ec7f41ad
Xavier Garcia <xgarcia@externo.wke.es>
2016-03-03 17:31:08 +0100

    Classes added.

---
+++ CleanCode.02/CleanCode.02/FilePathShortener.cs
@@ -0,0 +1,73 @@
+﻿namespace CleanCode.FileManteneinance
+{
+    using System;
+    using System.IO;
+    using System.Threading;
+
+    public class FilePathShortener
+    {
+        private readonly FoldersRenamer foldersRenamer;
+        private readonly FoldersRemover foldersRemover;
+        private string[] folderPaths;
+
+        public FilePathShortener()
+        {
+            this.foldersRenamer = new FoldersRenamer();
+            this.foldersRemover = new FoldersRemover();
+        }
+
+        public void ShortenFilePaths()
+        {
+            this.folderPaths = this.GetFolderPaths();
+
+            this.AddFolderPaths(folderPaths);
+            try
+            {
+                var renamedFolders = this.foldersRenamer.RenameFolders(folderPaths);
+
+                this.foldersRemover.TreatFoldersToRemove(renamedFolders);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occured:");
+                Console.WriteLine(e);
+            }
+
+            Console.ReadLine();
+        }
+
+        private string[] GetFolderPaths()
+        {
+            var folderPaths = new string[1];
+
+            Console.WriteLine("What directory do you want to shorten the content of?");
+
+            folderPaths[0] = Console.ReadLine();
+
+            return folderPaths;
+        }
+
+        private void AddFolderPaths(string[] folderPaths)
+        {
+            for (var pathIndex = 0; pathIndex < folderPaths.Length; pathIndex++)
+            {
+                folderPaths[pathIndex] = folderPaths[pathIndex].Trim('"');
+
+                if (folderPaths[pathIndex].IndexOfAny(Path.GetInvalidPathChars()) < 0)
+                {
+                    continue;
+                }
+
+                this.TreatPathNameError(folderPaths, pathIndex);
+            }
+        }
+
+        private void TreatPathNameError(string[] folderPaths, int pathIndex)
+        {
+            Console.WriteLine("Following Path contains invalid characters and will be ignored:");
+            Console.WriteLine(folderPaths[pathIndex]);
+
+            folderPaths[pathIndex] = null;
+        }
+    }
+}
---
+++ CleanCode.02/CleanCode.02/FoldersRenamer.cs
@@ -0,0 +1,75 @@
+﻿namespace CleanCode.FileManteneinance
+{
+    using System;
+    using System.IO;
+
+    public class FoldersRenamer
+    {
+        private static int foldersRenamed;
+        private static int foldersNotRenamed;
+
+        public string[] RenameFolders(string[] paths)
+        {
+            var newPaths = new string[paths.Length];
+            for (var indexInPaths= 0; indexInPaths < paths.Length; indexInPaths++)
+            {
+                newPaths[indexInPaths] = RenameFolder(paths[indexInPaths]);
+            }
+            return newPaths;
+        }
+
+        private static string RenameFolder(string folderPath)
+        {
+            if (string.IsNullOrEmpty(folderPath))
+            {
+                throw new Exception("A directory has to be specified.");
+            }
+
+            var attr = File.GetAttributes(folderPath);
+            if (!attr.HasFlag(FileAttributes.Directory))
+            {
+                throw new Exception("The specified path is a file.");
+            }
+
+            var folderInfo = new DirectoryInfo(folderPath);
+
+            if (!folderInfo.Exists)
+            {
+                throw new Exception("The directory doesn't exist.");
+            }
+
+            if (folderInfo.Parent != null)
+            {
+                var folderRenamedTo = folderInfo.Parent.FullName.TrimEnd('\\') + "\\" + "_" + foldersRenamed;
+                if (!Directory.Exists(folderRenamedTo))
+                {
+                    folderInfo.MoveTo(folderRenamedTo);
+                    foldersRenamed++;
+                }
+                else
+                {
+                    if (folderRenamedTo != folderInfo.FullName)
+                    {
+                        Console.WriteLine(
+                            "The following directory was not renamed because a conflict would have been caused:");
+                        Console.WriteLine(folderInfo.FullName);
+                    }
+
+                    foldersNotRenamed++;
+                }
+            }
+
+            var subdirectories = Directory.GetDirectories(folderInfo.FullName);
+            foreach (var subdirectory in subdirectories)
+            {
+                RenameFolder(subdirectory);
+            }
+
+            Console.WriteLine($"Success! Directories: {foldersRenamed + foldersNotRenamed}");
+            Console.WriteLine("Directories Renamed: " + foldersRenamed);
+            Console.WriteLine("Directories not renamed: " + foldersNotRenamed);
+            
+            return folderInfo.FullName;
+        }
+    }
+}
